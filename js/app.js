ad24992ac1b81a44f13d907f08629919fe40d9a8
= <lucas.lema.91@gmail.com>
2020-08-03 22:29:38 -0300

    Ejercicio

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Punto 1
+const Array1 = ['manzana', 3, {color: 'rojo'}, false, [3, 7, 1, true]]
+
+// Punto 2
+const Array2 = [2, 3, 1, 6, 3, 'hola', 5, 9, 1, 8, 7, '9', false, 4, 'chau', true, 'false', 'hola']
+
+// Punto 3
+let Set1 = new Set([3, 6, 9, 12, 15])
